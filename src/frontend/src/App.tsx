576d41ea080443dbeb01cfc05f653f8b27696d9b
Robin <robin.suxdorf@t-online.de>
2025-07-18 21:45:44 +0200

    Added middleware

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => setData(data))
+            .catch((error) => console.error("Fetch error:", error));
+    }, []);
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
     return (
     <>
         <p>
-            Hello World
+            {data ? JSON.stringify(data, null, 4) : "Loading..."}
         </p>
     </>
      )
